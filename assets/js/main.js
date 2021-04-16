c4ae48ccc75b55d7aab923e31a799bc015cfbf9c
Medy <meronhailekiros@gmail.com>
2021-04-16 16:16:43 -0400

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,7 @@ sr.reveal('.contact__input',{interval: 200});
 
 
 // work click event listenters 
-document.querySelectorAll(".work__img").addEventListener(click, "goToWork")
+document.querySelectorAll(".work__img").addEventListener("click", goToWork)
 
 function goToWork(){
     alert("works")
